109792ef07568a837a03d6806d2d2b5097493fff
rhumel <rhumel@hotmail.com>
2018-06-30 17:41:05 -0400

    added var questions with all the properties for the trivia

---
+++ assets/javascript/game.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+
+var correct =0;
+var wrong = 0;
+var intervalId;
+var clockRunning = false;
+var triviaQuest = [{ 
+      question: "What is the scientific name for the trunk of an elephant?",
+      A: "snufflelupagus",
+      B: "Probosis",
+      C: "luggage",
+      D: "snoutus",
+      correct: "B",
+      questImage: "./assets/images/elephant.jpg",
+},{
+      question: "Which ape gets its name from the Maly word meaning",
+      A: "Gorilla",
+      B: "Chimpanzee",
+      C: "Orangutan",
+      D: "Howler",
+      correct: "C",
+      questImage: "./assets/images/Orangutan.jpg",
+},{
+    question: "How is th domestic CAVY better known?",
+    A: "Guinea Pig",
+    B: "Mouse",
+    C: "Beaver",
+    D: "Rat",
+    correct: "A",
+    questImage: "./assets/images/Guineapig.jpg",
+},{
+    question: "What are the male honey bees called that are the only members of the colony allowed to to mate with the queen?",
+    A: "Worker",
+    B: "King",
+    C: "Hornet",
+    D: "Drone",
+    correct: "D",
+    questImage: "./assets/images/Bee.jpg",
+},{    
+    question: "What kind of animal is a karakul?",
+    A: "Baboon",
+    B: "Sheep",
+    C: "Lizard",
+    D: "Horse",
+    correct: "B",
+    questImage: "./assets/images/sheep.jpg",
+},{
+    question: "'Murder' is a collective noun for a group of of which bird?",
+    A: "Crow",
+    B: "Robin",
+    C: "Cardinal",
+    D: "Red Faced Warbler",
+    correct: "A",
+    questImage: "./assets/images/crow.jpg",
+},{
+    question: "What is a Cabbage White?",
+    A: "Moth",
+    B: "Finch",
+    C: "Butterfly",
+    D: "Snake",
+    correct: "C",
+    questImage: "./assets/images/butterfly.jpg",
+},{
+    question: "How many arms do most starfish have?",
+    A: "Six",
+    B: "Seven",
+    C: "Five",
+    D: "Eight",
+    correct: "A",
+    questImage: "./assets/images/starfish.jpg",
+},{ 
+    question: "From which animal is mohair obtained?",
+    A: "LLama",
+    B: "Horse",
+    C: "Sheep",
+    D: "Goat",
+    correct: "D",
+    questImage: "./assets/images/Goat.jpg",
+},{
+    question: "What kind of animal is a Flemish giant",
+    A: "Lion",
+    B: "Rabbit",
+    C: "Beaver",
+    D: "Cheetah",
+    correct: "B",
+    questImage: "./assets/images/rabbit.jpg",
+},{
+    }]
+
+
+// 
+
+
+
+
+
+
+});
